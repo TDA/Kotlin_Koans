b26164c1ae621740ddac5a1e69e67e719a2289df
Sai Chandramouli <schand31@asu.edu>
2017-11-12 10:48:33 -0800

    nullables

--- lesson1/task8/src/Task.kt
+++ lesson1/task8/src/Task.kt
@@ -1,7 +1,12 @@
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
 ){
-    TODO()
+
+    val personalInfo: PersonalInfo?  = client?.personalInfo
+    val email: String? = personalInfo?.email
+
+    if (email == null || message == null) return
+    mailer.sendMessage(email, message);
 }
 
 class Client (val personalInfo: PersonalInfo?)
