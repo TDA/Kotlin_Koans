77a594b8b498e5fb9e9bb986a5ba41bc97679b50
Sai Chandramouli <schand31@asu.edu>
2017-11-12 14:37:03 -0800

    object expressions, kinda not very intuitive

--- lesson1/task11/src/Task.kt
+++ lesson1/task11/src/Task.kt
@@ -2,6 +2,15 @@ import java.util.*
 
 fun getList(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, object {})
+    Collections.sort(arrayList, object: Comparator<Int> {
+        override fun compare(x: Int, y: Int): Int {
+            val retVal = when {
+                x == y -> 0
+                x > y -> -1
+                else -> 1
+            }
+            return retVal
+        }
+    })
     return arrayList
 }
