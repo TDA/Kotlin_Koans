9099d3ed912bd5a0149fe499d32650c47a273800
Sai Chandramouli <schand31@asu.edu>
2017-11-12 14:47:24 -0800

    SAM is a reduced form of using object expressions for Abstract methods

--- lesson1/task12/src/Task.kt
+++ lesson1/task12/src/Task.kt
@@ -2,6 +2,10 @@ import java.util.*
 
 fun getList(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, { x, y -> TODO() })
+    Collections.sort(arrayList, { x, y -> when {
+        x == y -> 0
+        x > y -> -1
+        else -> 1
+    } })
     return arrayList
 }
