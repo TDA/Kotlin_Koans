e76d838b3dfe985a5f52578e9cdb5b731e40fad2
Sai Chandramouli <schand31@asu.edu>
2017-11-12 16:10:05 -0800

    built in operator overrides

--- lesson2/task1/src/Task.kt
+++ lesson2/task1/src/Task.kt
@@ -1,5 +1,13 @@
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    /* TODO */
+    override fun compareTo(other: MyDate): Int {
+        return when {
+            this.year == other.year && this.month == other.month && this.dayOfMonth == other.dayOfMonth -> 0
+            this.year == other.year && this.month == other.month && (this.dayOfMonth > other.dayOfMonth) ||
+                    (this.year == other.year && (this.month > other.month)) ||
+                    (this.year > other.year) -> 1
+            else -> -1
+        }
+    }
 }
 
 fun compare(date1: MyDate, date2: MyDate) = date1 < date2
